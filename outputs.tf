3250882086473dcfa9f00c0ccbe8bcd15932112c
Daniel Allen <dan@aeternagroup.net>
2026-08-15 02:17:50 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "vpclattice_target_group_attachments_region" {
 }
 output "vpclattice_target_group_attachments_target" {
   description = "Map of target values across all vpclattice_target_group_attachments, keyed the same as var.vpclattice_target_group_attachments"
-  value       = { for k, v in aws_vpclattice_target_group_attachment.vpclattice_target_group_attachments : k => v.target if v.target != null && length(v.target) > 0 }
+  value       = { for k, v in aws_vpclattice_target_group_attachment.vpclattice_target_group_attachments : k => one(v.target) if v.target != null && length(v.target) > 0 }
 }
 output "vpclattice_target_group_attachments_target_group_identifier" {
   description = "Map of target_group_identifier values across all vpclattice_target_group_attachments, keyed the same as var.vpclattice_target_group_attachments"
